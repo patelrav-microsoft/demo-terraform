a0cee39ffc213c548d6fdca7e65bb8ec5a9374cc
Ravi Patel <patelrav@microsoft.com>
2026-04-01 16:43:57 -0500

    updated resource group

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-fordresource-group"
+    resource_group_name  = "demo-fordrg-1"
     storage_account_name = "demofordsgacc123"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
